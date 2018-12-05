4275af081e85116ea4189ad4d4d822beeb2f3b46
Zoe O'Connell <zoe@complicity.co.uk>
2018-12-05 08:03:56 +0000

    Day five

---
+++ aoc2018/day05.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+'''Advent of Code 2018 Day 5 solution'''
+from string import ascii_lowercase
+from typing import Tuple
+
+def runpart1(inputs: str) -> int:
+    '''Solve part 1'''
+    i = 0
+
+    # Output
+    o = ''
+    # Loop is called often enough that precalculating this saves time!
+    l = len(inputs)-1
+
+    # Loop across input characters
+    while i < l:
+        # If the current and next character would react, skip both.
+        if inputs[i] != inputs[i+1] and inputs[i].lower() == inputs[i+1].lower():
+            i += 2
+            try:
+                # It's possible the last character on the output buffer reacts with out next
+                # character. If so, skip the next character and remove the existing one from
+                # the output buffer.
+                while inputs[i] != o[-1:] and inputs[i].lower() == o[-1:].lower():
+                    i += 1
+                    o = o[:-1]
+            # We might have gone beyond the end of our input, so catch that.
+            except IndexError:
+                pass
+        else:
+            # Nothing reacts, so add to the output and advance to the next input character
+            o += inputs[i]
+            i += 1
+
+    # We probably have one character left in the input, so add it to the output if so.
+    try:
+        o += inputs[i]
+    except IndexError:
+        pass
+
+    return len(o)
+
+def runpart2(inputs: str) -> int:
+    '''Solve part 2'''
+    m = len(inputs)
+
+    # Remove every character a-z in turn, and run the part 1 solution. Keep track of our best
+    # (i.e. lowest) score in m as we go along.
+    for c in ascii_lowercase:
+        s = inputs.replace(c, '')
+        s = s.replace(c.upper(), '')
+        result = runpart1(s)
+        if result < m:
+            m = result
+
+    return m
+
+def run() -> Tuple[int, int]:
+    '''Main'''
+
+    # Read input data
+    with open('inputs/day05.txt', 'r') as f:
+        inputs = f.read().rstrip("\n")
+
+    # Solve the problem
+    part1 = runpart1(inputs)
+    part2 = runpart2(inputs)
+
+    return (part1, part2)
+
+if __name__ == '__main__':
+    print(run())
---
+++ tests/day05_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Advent of Code 2018 Day 4 tests'''
+import unittest
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from aoc2018 import day05 # pylint: disable=wrong-import-position
+
+class TestUM(unittest.TestCase):
+    '''Unit Tests'''
+    def test_day05part1(self) -> None:
+        '''Test part 1'''
+        s = 'dabAcCaCBAcCcaDA'
+        self.assertEqual(day05.runpart1(s), 10)
+
+    def test_day05part2(self) -> None:
+        '''Test part 2'''
+        s = 'dabAcCaCBAcCcaDA'
+        self.assertEqual(day05.runpart2(s), 4)
+
+    def test_day05run(self) -> None:
+        '''Find the actual solution'''
+        _ = self
+
+        with open('results.txt', 'a') as f:
+            f.write("Day 05: %d, %d\n" % day05.run())
